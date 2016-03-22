3d5efa691f7660633c34f6bb4c588a48006b4f21
lh <beyondlh@126.com>
2016-03-22 22:03:59 +0800

    添加注释

--- src/main/java/com/websystique/springmvc/controller/FileUploadController.java
+++ src/main/java/com/websystique/springmvc/controller/FileUploadController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.FileCopyUtils;
@@ -26,89 +27,90 @@ import com.websystique.springmvc.util.MultiFileValidator;
 @Controller
 public class FileUploadController {
 
-	private static String UPLOAD_LOCATION="C:/mytemp/";
-	
-	@Autowired
-	FileValidator fileValidator;
-	
 
-	@Autowired
-	MultiFileValidator multiFileValidator;
+    private static String UPLOAD_LOCATION = "E:/mytemp/";
 
-	
-	@InitBinder("fileBucket")
-	protected void initBinderFileBucket(WebDataBinder binder) {
-	   binder.setValidator(fileValidator);
-	}
+    @Autowired
+    FileValidator fileValidator;
 
 
-	@InitBinder("multiFileBucket")
-	protected void initBinderMultiFileBucket(WebDataBinder binder) {
-	   binder.setValidator(multiFileValidator);
-	}
+    @Autowired
+    MultiFileValidator multiFileValidator;
 
-	
-	@RequestMapping(value={"/","/welcome"}, method = RequestMethod.GET)
-	public String getHomePage(ModelMap model) {
-		return "welcome";
-	}
 
-	@RequestMapping(value="/singleUpload", method = RequestMethod.GET)
-	public String getSingleUploadPage(ModelMap model) {
-		FileBucket fileModel = new FileBucket();
-		model.addAttribute("fileBucket", fileModel);
-		return "singleFileUploader";
-	}
+    @InitBinder("fileBucket")
+    protected void initBinderFileBucket(WebDataBinder binder) {
+        binder.setValidator(fileValidator);
+    }
 
-	@RequestMapping(value="/singleUpload", method = RequestMethod.POST)
-	public String singleFileUpload(@Valid FileBucket fileBucket, BindingResult result, ModelMap model) throws IOException {
 
-		if (result.hasErrors()) {
-			System.out.println("validation errors");
-			return "singleFileUploader";
-		} else {			
-			System.out.println("Fetching file");
-			MultipartFile multipartFile = fileBucket.getFile();
+    @InitBinder("multiFileBucket")
+    protected void initBinderMultiFileBucket(WebDataBinder binder) {
+        binder.setValidator(multiFileValidator);
+    }
 
-			//Now do something with file...
-			FileCopyUtils.copy(fileBucket.getFile().getBytes(), new File(UPLOAD_LOCATION + fileBucket.getFile().getOriginalFilename()));
-			
-			String fileName = multipartFile.getOriginalFilename();
-			model.addAttribute("fileName", fileName);
-			return "success";
-		}
-	}
 
-	
-	@RequestMapping(value="/multiUpload", method = RequestMethod.GET)
-	public String getMultiUploadPage(ModelMap model) {
-		MultiFileBucket filesModel = new MultiFileBucket();
-		model.addAttribute("multiFileBucket", filesModel);
-		return "multiFileUploader";
-	}
+    @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
+    public String getHomePage(ModelMap model) {
+        return "welcome";
+    }
 
-	@RequestMapping(value="/multiUpload", method = RequestMethod.POST)
-	public String multiFileUpload(@Valid MultiFileBucket multiFileBucket, BindingResult result, ModelMap model) throws IOException {
+    @RequestMapping(value = "/singleUpload", method = RequestMethod.GET)
+    public String getSingleUploadPage(ModelMap model) {
+        FileBucket fileModel = new FileBucket();
+        model.addAttribute("fileBucket", fileModel);
+        return "singleFileUploader";
+    }
 
-		
-		if (result.hasErrors()) {
-			System.out.println("validation errors in multi upload");
-			return "multiFileUploader";
-		} else {			
-			System.out.println("Fetching files");
-			List<String> fileNames= new ArrayList<String>();
-			
-			//Now do something with file...
-			for(FileBucket bucket : multiFileBucket.getFiles()){
-				FileCopyUtils.copy(bucket.getFile().getBytes(), new File(UPLOAD_LOCATION + bucket.getFile().getOriginalFilename()));
-				fileNames.add(bucket.getFile().getOriginalFilename());
-			}
-			
-			model.addAttribute("fileNames", fileNames);
-			return "multiSuccess";
-		}
-	}
-	
-	
-	
+    @RequestMapping(value = "/singleUpload", method = RequestMethod.POST)
+    public String singleFileUpload(@Valid FileBucket fileBucket, BindingResult result, ModelMap model) throws IOException {
+
+        if (result.hasErrors()) {
+            System.out.println("validation errors");
+            return "singleFileUploader";
+        } else {
+            System.out.println("Fetching file");
+            MultipartFile multipartFile = fileBucket.getFile();
+
+            //Now do something with file...
+            if (!(new File(UPLOAD_LOCATION).isDirectory())) {
+                new File(UPLOAD_LOCATION).mkdir();
+            }
+            FileCopyUtils.copy(fileBucket.getFile().getBytes(), new File(UPLOAD_LOCATION + fileBucket.getFile().getOriginalFilename()));
+
+            String fileName = multipartFile.getOriginalFilename();
+            model.addAttribute("fileName", fileName);
+            return "success";
+        }
+    }
+
+
+    @RequestMapping(value = "/multiUpload", method = RequestMethod.GET)
+    public String getMultiUploadPage(ModelMap model) {
+        MultiFileBucket filesModel = new MultiFileBucket();
+        model.addAttribute("multiFileBucket", filesModel);
+        return "multiFileUploader";
+    }
+
+    @RequestMapping(value = "/multiUpload", method = RequestMethod.POST)
+    public String multiFileUpload(@Valid MultiFileBucket multiFileBucket, BindingResult result, ModelMap model) throws IOException {
+
+
+        if (result.hasErrors()) {
+            System.out.println("validation errors in multi upload");
+            return "multiFileUploader";
+        } else {
+            System.out.println("Fetching files");
+            List<String> fileNames = new ArrayList<String>();
+
+            //Now do something with file...
+            for (FileBucket bucket : multiFileBucket.getFiles()) {
+                FileCopyUtils.copy(bucket.getFile().getBytes(), new File(UPLOAD_LOCATION + bucket.getFile().getOriginalFilename()));
+                fileNames.add(bucket.getFile().getOriginalFilename());
+            }
+
+            model.addAttribute("fileNames", fileNames);
+            return "multiSuccess";
+        }
+    }
 }
--- src/main/java/com/websystique/springmvc/util/FileValidator.java
+++ src/main/java/com/websystique/springmvc/util/FileValidator.java
@@ -8,18 +8,18 @@ import com.websystique.springmvc.model.FileBucket;
 
 @Component
 public class FileValidator implements Validator {
-	
-	public boolean supports(Class<?> clazz) {
-		return FileBucket.class.isAssignableFrom(clazz);
-	}
 
-	public void validate(Object obj, Errors errors) {
-		FileBucket file = (FileBucket) obj;
-		
-		if(file.getFile()!=null){
-			if (file.getFile().getSize() == 0) {
-				errors.rejectValue("file", "missing.file");
-			}
-		}
-	}
+    public boolean supports(Class<?> clazz) {
+        return FileBucket.class.isAssignableFrom(clazz);
+    }
+
+    public void validate(Object obj, Errors errors) {
+        FileBucket file = (FileBucket) obj;
+        
+        if (file.getFile() != null) {
+            if (file.getFile().getSize() == 0) {
+                errors.rejectValue("file", "missing.file");
+            }
+        }
+    }
 }
